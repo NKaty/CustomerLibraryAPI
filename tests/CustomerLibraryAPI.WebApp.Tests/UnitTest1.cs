9fbe11d32bf466f31a61aec8109d8e441c187f3b
Ekaterina N <nd.katy@gmail.com>
2021-07-07 19:11:23 +0300

    Add project files.

---
+++ tests/CustomerLibraryAPI.WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomerLibraryAPI.WebApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
